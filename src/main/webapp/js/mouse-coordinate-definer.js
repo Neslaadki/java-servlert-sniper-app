2026a497379a716bd1cb19e74626563533981960
Neslaadki <nesladkiy@icloud.com>
2020-10-18 18:51:46 +0300

    Дизигн 2.0 Крутые радиусы

--- src/main/webapp/js/mouse-coordinate-definer.js
+++ src/main/webapp/js/mouse-coordinate-definer.js
@@ -25,12 +25,14 @@ let radius_is_installed = document.querySelector('#r_value').onclick = function 
 
 document.querySelector('#svg_wrapper').onclick = function (event){
     if (radius_is_installed() === 1){
-        let clickX = String(event.offsetX);
-        let clickY = String(event.offsetY);
-        let a = "?x_value=" + clickX + "&y_value=" + clickY + "&r_value=" + String(radiusValue);
+        let clickX = Number(event.offsetX) - 150;
+        let clickY = Number(event.offsetY) - 150;
+        let x_val = ((radiusValue * clickX)/120).toFixed(3);
+        let y_val = ((-1) * (radiusValue * clickY)/120).toFixed(3);
+        let a = "?x_value=" + String(x_val) + "&y_value=" + String(y_val) + "&r_value=" + String(radiusValue);
         document.location.href = '/web_2-1.0-SNAPSHOT/webServlet' + a;
     }else{
-        alert('R ВЫБЕРИ!');
+        alert('Необходимо задать значение R!');
     }
 }
 
